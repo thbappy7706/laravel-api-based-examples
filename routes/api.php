4a08e66d7340655f58d82dcf10ecb6f7251bb82f
tanvir.bappy <tanvir.bappy@bansberrysg.com>
2025-12-30 15:08:02 +0600

    api working

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('test-posts', function () {
+    $data = collect(Http::get('https://jsonplaceholder.typicode.com/posts')->json())->take(50);
+    return $data->map(function ($item) {
+        unset($item['userId']);
+        return $item;
+    });
+});
